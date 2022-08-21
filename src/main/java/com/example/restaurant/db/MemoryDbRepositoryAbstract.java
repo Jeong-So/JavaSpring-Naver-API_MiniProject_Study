d2be19cd74c96ab25045dfa193eaf5a8f4b94ab0
Jeong-So <83945245+Jeong-So@users.noreply.github.com>
2022-08-22 00:03:15 +0900

    Project를 위한 Memory DB(CRUD) 만들기: not use JPA

---
+++ src/main/java/com/example/restaurant/db/MemoryDbRepositoryAbstract.java
@@ -0,0 +1,59 @@
+package com.example.restaurant.db;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+abstract public class MemoryDbRepositoryAbstract<T extends MemoryDbEntity> implements MemoryDbRepositoryIfs<T> {
+
+    private final List<T> db = new ArrayList<>();
+    private int index = 0;
+
+    @Override
+    public Optional<T> findById(int index) {
+        return db.stream().filter(it -> it.getIndex() == index).findFirst();
+        // db stream()에 들어있는 List<T> db 타입에 대한부분
+        // it.getIndex() : MemoryDbEntity에 정의된 index를 의미
+    }
+
+    @Override
+    public T save(T entity) {
+
+        var optionalEntity = db.stream().filter(it -> it.getIndex() == entity.getIndex()).findFirst();
+
+        if(optionalEntity.isEmpty()){
+            // db에 데이터가 없는경우
+            index++;
+            entity.setIndex(index);
+            db.add(entity);
+
+            return entity;
+        }else{
+            // db에 이미 데이터가 있는 경우
+            var preIndex  = optionalEntity.get().getIndex();
+            entity.setIndex(preIndex);
+
+            deleteById(preIndex);
+            db.add(entity);
+
+            return entity;
+        }
+
+    }
+
+    @Override
+    public void deleteById(int index) {
+
+        var optionalEntity = db.stream().filter(it -> it.getIndex() == index).findFirst();
+        if(optionalEntity.isPresent()){
+            // db에 이미 데이터가 있는 경우
+            db.remove(optionalEntity.get());
+        }
+    }
+
+    @Override
+    public List<T> listAll() {
+        return db;
+    }
+
+}
